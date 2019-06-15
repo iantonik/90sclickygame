b3d4bc2e1cc06c6987bdbbf4dd22fc7dbf95a89f
Iwona Antonik <iwona.antonik@gmail.com>
2019-06-15 13:00:15 -0400

    Updates

---
+++ precache-manifest.3ceed1370730eef53b01e2dd832ee413.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497b1e615a48bd9fe4ef053b10390b5a",
+    "url": "/90sclickygame/index.html"
+  },
+  {
+    "revision": "883ffe91a1b437ec5943",
+    "url": "/90sclickygame/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/90sclickygame/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "883ffe91a1b437ec5943",
+    "url": "/90sclickygame/static/js/main.eb3b0aeb.chunk.js"
+  },
+  {
+    "revision": "f87eebc38ccf69a8eb7d",
+    "url": "/90sclickygame/static/js/runtime~main.f9f37b73.js"
+  }
+]);
